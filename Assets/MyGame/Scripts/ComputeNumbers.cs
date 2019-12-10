2e8220db5af9a6c20aa84b847f413f8ec576ef8a
tguerel <tateguerel@gmail.com>
2019-12-10 16:28:12 +0100

    updated script

---
+++ Assets/MyGame/Scripts/ComputeNumbers.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputeNumbers : MonoBehaviour
+{ 
+    public Text result;
+    public InputField varA;
+    public InputField varB;
+    public Button btn_reset;
+    public Button btn_subtract_Numbers;
+
+    private void Start()
+    {
+        btn_reset.interactable = false;
+    }
+
+    public void SetResult()
+    {
+        result.text = SubtractNumbers().ToString();
+        varA.interactable = false;
+        varB.interactable = false;
+        btn_subtract_Numbers.interactable = false;
+        btn_reset.interactable = true;
+    }
+
+    public void Reset()
+    {
+        varA.text = "0";
+        varB.text = "0";
+        varA.interactable = true;
+        varB.interactable = true;
+        btn_subtract_Numbers.interactable = true;
+        btn_reset.interactable = false;
+        result.text = "Result";
+    }
+    private int SubtractNumbers()
+    {
+        int tempResult = int.Parse(varA.text) - int.Parse(varB.text);
+        return tempResult;
+    }
+}
